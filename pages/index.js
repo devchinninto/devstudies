20516699d200bd48f9a1f5c61c419dd3af3ff713
Marcelle Alves <marcellealves018@gmail.com>
2025-12-05 18:18:16 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Cada minuto de dedicação agora, mesmo com medo, vai te levar a lugares incríveis e te trazer pra perto pessoas que você nem imaginaria em conhecer. Confia no processo, e dá um passo de cada vez. Bora em frente :D</h1>
+}
+
+export default Home
