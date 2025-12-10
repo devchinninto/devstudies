6d1c3ba3eb31d8faf4815168ca2cbc37f07be491
Marcelle Alves <marcellealves018@gmail.com>
2025-12-10 16:08:38 -0300

    atualiza mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Cada minuto de dedicação agora, mesmo com medo, vai te levar a lugares incríveis e te trazer pra perto pessoas que você nem imaginaria em conhecer. Confia no processo, e dá um passo de cada vez. Bora em frente :D</h1>
+  return <h1>Muri, cada minuto de dedicação agora, mesmo com medo, vai te levar a lugares incríveis e te trazer pra perto pessoas que você nem imaginaria em conhecer. Confia no processo e dá um passo de cada vez. Bora em frente 😎</h1>
 }
 
 export default Home
